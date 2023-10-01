272198bd761f2a60120b781dcea0db2ec9f22e55
Arafat Hossain Ar <arafat.122260@gmail.com>
2023-10-01 17:29:48 +0600

    update app.js, index.html and style.css

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 const app = Vue.createApp({
     data() {
         return {
-            firstName: 'John',
-            lastName: 'Doe',
-            email: 'John@gmail.com',
+            title: 'Mr',
+            firstName: 'Arafat',
+            lastName: 'Hossain',
+            email: 'arafat.122260@gmail.com',
+            phone: '01789404345',
             gender: 'male',
-            picture: 'https://randomuser.me/api/portraits/men/11.jpg'
+            age: 24,
+            city: 'Dhaka',
+            state: 'Dhaka',
+            country: 'Bangladesh',
+            picture: 'https://avatars.githubusercontent.com/u/26932301?v=4'
         }
     },
     methods: {
-       async getUser() {
-        const res = await fetch('https://randomuser.me/api')
-            this.firstName = 'Lara'
-            this.lastName = 'Doe'
-            this.email = 'Lara@gmail.com'
-            this.gender = 'female',
-            this.picture = 'https://randomuser.me/api/portraits/women/12.jpg'
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json()
+            this.title = results[0].name.title
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.phone = results[0].phone
+            this.gender = results[0].gender
+            this.age = results[0].dob.age
+            this.city = results[0].location.city
+            this.state = results[0].location.state
+            this.country = results[0].location.country
+            this.picture = results[0].picture.large
         }
     }
 })
